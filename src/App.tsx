49fd2a520defc74cc1205c9162cee84653f2818b
Junior Paula <slucivaldo509@gmail.com>
2022-05-02 20:22:03 -0300

    feat: add Widget Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
+import { Widget } from './components/Widget';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return <h1>Hello word!</h1>
+export function App () {
+  return <Widget />
 }
-
-export default App
